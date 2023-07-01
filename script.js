5a8be176c70461b870ddba4e7461717844e2f88c
Shivam <133050824+shi255vam@users.noreply.github.com>
2023-07-01 15:11:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const currentTime = document.querySelector("h1"),
 // "setAlarmBtn" selects the button used to set and clear the alarm.
 
 let alarmTime, isAlarmSet,
-    ringtone = new Audio("C:\Users\shivam\OneDrive\Desktop\New folder\files_ringtone.mp3");
+    ringtone = new Audio("Crow Itachi.mp3");
 
 // The variables above are used to track the alarm time, if an alarm is set, and the ringtone.
 
